39b2d818dee5a6809a4aceceacfe0a2a244b20ad
Shashwat Pattanayak <shashwatpattanayak.2020@vitbhopal.ac.in>
2023-08-11 23:46:14 +0530

    Time: 0 ms (100.00%), Space: 39.2 MB (89.46%) - LeetHub

---
+++ 0868-binary-gap/0868-binary-gap.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int binaryGap(int n) {
+        int ans = 0;
+        
+        for(int d=-32; n>0; n/=2, ++d){
+            if((n&1) == 1){
+                ans = Math.max(ans,d);
+                d=0;
+            }
+        }
+        return ans;
+        
+        
+    }
+}
