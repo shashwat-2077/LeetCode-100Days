7d60e8e29eac8edd9d2914c9d193db547b11829f
Shashwat Pattanayak <shashwatpattanayak.2020@vitbhopal.ac.in>
2023-08-18 21:45:58 +0530

    Time: 1 ms (100.00%), Space: 40.9 MB (78.65%) - LeetHub

--- 1594-maximum-non-negative-product-in-a-matrix/1594-maximum-non-negative-product-in-a-matrix.java
+++ 1594-maximum-non-negative-product-in-a-matrix/1594-maximum-non-negative-product-in-a-matrix.java
@@ -1,23 +1,21 @@
 class Solution {
   public int maxProductPath(int[][] grid) {
-    final int kMod = 1_000_000_007;
-    final int m = grid.length;
-    final int n = grid[0].length;
-    // dpMin[i][j] := the min product from (0, 0) to (i, j)
-    // dpMax[i][j] := the max product from (0, 0) to (i, j)
-    long[][] dpMin = new long[m][n];
-    long[][] dpMax = new long[m][n];
+    final int mod = 1_000_000_007;
+    final int rows = grid.length;
+    final int columns = grid[0].length;
+    long[][] dpMin = new long[rows][columns];
+    long[][] dpMax = new long[rows][columns];
 
     dpMin[0][0] = dpMax[0][0] = grid[0][0];
 
-    for (int i = 1; i < m; ++i)
+    for (int i = 1; i < rows; ++i)
       dpMin[i][0] = dpMax[i][0] = dpMin[i - 1][0] * grid[i][0];
 
-    for (int j = 1; j < n; ++j)
+    for (int j = 1; j < columns; ++j)
       dpMin[0][j] = dpMax[0][j] = dpMin[0][j - 1] * grid[0][j];
 
-    for (int i = 1; i < m; ++i)
-      for (int j = 1; j < n; ++j)
+    for (int i = 1; i < rows; ++i)
+      for (int j = 1; j < columns; ++j)
         if (grid[i][j] < 0) {
           dpMin[i][j] = Math.max(dpMax[i - 1][j], dpMax[i][j - 1]) * grid[i][j];
           dpMax[i][j] = Math.min(dpMin[i - 1][j], dpMin[i][j - 1]) * grid[i][j];
@@ -26,7 +24,7 @@ class Solution {
           dpMax[i][j] = Math.max(dpMax[i - 1][j], dpMax[i][j - 1]) * grid[i][j];
         }
 
-    final long max = Math.max(dpMin[m - 1][n - 1], dpMax[m - 1][n - 1]);
-    return max < 0 ? -1 : (int) (max % kMod);
+    final long max = Math.max(dpMin[rows - 1][columns - 1], dpMax[rows - 1][columns - 1]);
+    return max < 0 ? -1 : (int) (max % mod);
   }
 }
